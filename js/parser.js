0e10e9915639df3ef2cdcf254d666425ac0ef965
Markus Stange <mstange@themasta.com>
2012-03-19 23:10:55 +0100

    Use a worker for call tree construction. Needs business UI.

--- js/parser.js
+++ js/parser.js
@@ -12,45 +12,6 @@ function cloneSample(sample) {
   return makeSample(sample.frames.clone(), sample.extraInfo, sample.lines.clone());
 }
 
-function TreeNode(name, parent, startCount) {
-  this.name = name;
-  this.children = [];
-  this.counter = startCount;
-  this.parent = parent;
-}
-TreeNode.prototype.getDepth = function TreeNode__getDepth() {
-  if (this.parent)
-    return this.parent.getDepth() + 1;
-  return 0;
-};
-TreeNode.prototype.findChild = function TreeNode_findChild(name) {
-  for (var i = 0; i < this.children.length; i++) {
-    var child = this.children[i];
-    if (child.name == name)
-      return child;
-  }
-  return null;
-}
-// path is an array of strings which is matched to our nodes' names.
-// Try to walk path in our own tree and return the last matching node. The
-// length of the match can be calculated by the caller by comparing the
-// returned node's depth with the depth of the path's start node.
-TreeNode.prototype.followPath = function TreeNode_followPath(path) {
-  if (path.length == 0)
-    return this;
-
-  var matchingChild = this.findChild(path[0]);
-  if (!matchingChild)
-    return this;
-
-  return matchingChild.followPath(path.slice(1));
-};
-TreeNode.prototype.incrementCountersInParentChain = function TreeNode_incrementCountersInParentChain() {
-  this.counter++;
-  if (this.parent)
-    this.parent.incrementCountersInParentChain();
-};
-
 var gParserWorker = new Worker("js/parserWorker.js");
 gParserWorker.nextRequestID = 0;
 
@@ -185,33 +146,20 @@ var Parser = {
     };
   },
 
-  convertToCallTree: function Parser_convertToCallTree(profile, isReverse, callback) {
-    var samples = profile.samples.filter(function noNullSamples(sample) {
-      return sample != null;
-    });
-    if (samples.length == 0)
-      return new TreeNode("(empty)", null, 0);
-    var treeRoot = new TreeNode(isReverse ? "(total)" : samples[0].frames[0], null, 0);
-    for (var i = 0; i < samples.length; ++i) {
-      var sample = samples[i];
-      var callstack = sample.frames.clone();
-      callstack.shift();
-      if (isReverse)
-        callstack.reverse();
-      var deepestExistingNode = treeRoot.followPath(callstack);
-      var remainingCallstack = callstack.slice(deepestExistingNode.getDepth());
-      deepestExistingNode.incrementCountersInParentChain();
-      var node = deepestExistingNode;
-      for (var j = 0; j < remainingCallstack.length; ++j) {
-        var frame = remainingCallstack[j];
-        var child = new TreeNode(frame, node, 1);
-        node.children.push(child);
-        node = child;
+  convertToCallTree: function Parser_convertToCallTree(profile, isReverse, finishCallback) {
+    var requestID = gParserWorker.nextRequestID++;
+    gParserWorker.addEventListener("message", function onMessageFromWorker(msg) {
+      if (msg.data.requestID == requestID) {
+        gParserWorker.removeEventListener("message", onMessageFromWorker);
+        finishCallback(msg.data.calltree);
       }
-    }
-    setTimeout(function () {
-      callback(treeRoot);
-    }, 0);
+    });
+    gParserWorker.postMessage({
+      requestID: requestID,
+      task: "convertToCallTree",
+      profile: profile,
+      isReverse: isReverse
+    });
   },
   _clipText: function Tree__clipText(text, length) {
     if (text.length <= length)
--- js/parserWorker.js
+++ js/parserWorker.js
@@ -4,6 +4,9 @@ self.onmessage = function (msg) {
       case "parseRawProfile":
         parseRawProfile(msg.data.requestID, msg.data.rawProfile);
         break;
+      case "convertToCallTree":
+        convertToCallTree(msg.data.requestID, msg.data.profile, msg.data.isReverse)
+        break;
     }
   } catch (e) {
     dump("Exception: " + e + "\n");
@@ -45,7 +48,7 @@ function parseRawProfile(requestID, rawProfile) {
   }
 
   function cloneSample(sample) {
-    return makeSample(sample.frames.clone(), sample.extraInfo, sample.lines.clone());
+    return makeSample(sample.frames.slice(0), sample.extraInfo, sample.lines.clone());
   }
 
   function cleanFunctionName(functionName) {
@@ -154,5 +157,73 @@ function parseRawProfile(requestID, rawProfile) {
   self.postMessage({
     requestID: requestID,
     parsedProfile: { symbols: symbols, functions: functions, samples: samples}
+  });
+}
+function TreeNode(name, parent, startCount) {
+  this.name = name;
+  this.children = [];
+  this.counter = startCount;
+  this.parent = parent;
+}
+TreeNode.prototype.getDepth = function TreeNode__getDepth() {
+  if (this.parent)
+    return this.parent.getDepth() + 1;
+  return 0;
+};
+TreeNode.prototype.findChild = function TreeNode_findChild(name) {
+  for (var i = 0; i < this.children.length; i++) {
+    var child = this.children[i];
+    if (child.name == name)
+      return child;
+  }
+  return null;
+}
+// path is an array of strings which is matched to our nodes' names.
+// Try to walk path in our own tree and return the last matching node. The
+// length of the match can be calculated by the caller by comparing the
+// returned node's depth with the depth of the path's start node.
+TreeNode.prototype.followPath = function TreeNode_followPath(path) {
+  if (path.length == 0)
+    return this;
+
+  var matchingChild = this.findChild(path[0]);
+  if (!matchingChild)
+    return this;
+
+  return matchingChild.followPath(path.slice(1));
+};
+TreeNode.prototype.incrementCountersInParentChain = function TreeNode_incrementCountersInParentChain() {
+  this.counter++;
+  if (this.parent)
+    this.parent.incrementCountersInParentChain();
+};
+
+function convertToCallTree(requestID, profile, isReverse) {
+  var samples = profile.samples.filter(function noNullSamples(sample) {
+    return sample != null;
+  });
+  if (samples.length == 0)
+    return new TreeNode("(empty)", null, 0);
+  var treeRoot = new TreeNode(isReverse ? "(total)" : samples[0].frames[0], null, 0);
+  for (var i = 0; i < samples.length; ++i) {
+    var sample = samples[i];
+    var callstack = sample.frames.slice(0);
+    callstack.shift();
+    if (isReverse)
+      callstack.reverse();
+    var deepestExistingNode = treeRoot.followPath(callstack);
+    var remainingCallstack = callstack.slice(deepestExistingNode.getDepth());
+    deepestExistingNode.incrementCountersInParentChain();
+    var node = deepestExistingNode;
+    for (var j = 0; j < remainingCallstack.length; ++j) {
+      var frame = remainingCallstack[j];
+      var child = new TreeNode(frame, node, 1);
+      node.children.push(child);
+      node = child;
+    }
+  }
+  self.postMessage({
+    requestID: requestID,
+    calltree: treeRoot
   });
 }
