9bc5dc448c04a53fe4d842a4fac0f3fd3a498be1
Benoit Girard <b56girard@gmail.com>
2012-06-11 16:59:56 -0400

    Add CC diag

--- js/diagnosticBar.js
+++ js/diagnosticBar.js
@@ -5,6 +5,7 @@ var diagnosticList = [
     check: function(frames, symbols) {
       return stepContains('GCCycle', frames, symbols)
           || stepContains('GarbageCollectNow', frames, symbols) // Label
+          || stepContains('CycleCollect', frames, symbols) // Label
           ;
     },
   },
