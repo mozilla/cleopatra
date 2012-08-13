d8f155dfa4994729ddad11d49e4280a2f6a4b024
Benoit Girard <b56girard@gmail.com>
2012-08-13 09:50:46 -0700

    Add button bar for source view

--- js/sourceView.js
+++ js/sourceView.js
@@ -4,6 +4,8 @@ function SourceView() {
   this._container = document.createElement("div");
   this._container.className = "sourceViewContainer";
 
+  this._buttonBar = document.createElement("div");
+  this._buttonBar.className = "sourceViewTrail";
   this._closeButton = document.createElement("div");
   this._closeButton.className = "sourceViewTrailItem";
   this._closeButton.innerHTML = "[X] Close";
@@ -11,7 +13,8 @@ function SourceView() {
   this._closeButton.onclick = function() {
     self._container.parentNode.removeChild(self._container);
   }
-  this._container.appendChild(this._closeButton);
+  this._buttonBar.appendChild(this._closeButton);
+  this._container.appendChild(this._buttonBar);
 }
 
 SourceView.prototype = {
