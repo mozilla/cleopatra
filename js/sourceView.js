84733b8752a142ebf2d1bd18efc444c91a3177e5
Benoit Girard <b56girard@gmail.com>
2012-08-13 09:47:46 -0700

    Add close button

--- js/sourceView.js
+++ js/sourceView.js
@@ -4,12 +4,23 @@ function SourceView() {
   this._container = document.createElement("div");
   this._container.className = "sourceViewContainer";
 
-  this._container.innerHTML = "View source test";
+  this._closeButton = document.createElement("div");
+  this._closeButton.className = "sourceViewTrailItem";
+  this._closeButton.innerHTML = "[X] Close";
+  var self = this;
+  this._closeButton.onclick = function() {
+    self._container.parentNode.removeChild(self._container);
+  }
+  this._container.appendChild(this._closeButton);
 }
 
 SourceView.prototype = {
   getContainer: function SourceView_getContainer() {
     return this._container;
   },
+
+  setSource: function SourceView_setSource(source) {
+    this._source = source; 
+  }
 }
 
