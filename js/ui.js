a272d4f85cfdaca3736b96b6d5df90d56742d481
Matthew Noorenberghe <github@matthew.noorenberghe.com>
2012-09-22 17:21:38 -0700

    infoBar improvements

--- js/ui.js
+++ js/ui.js
@@ -895,7 +895,7 @@ function effectiveInterval() {
   if (effectiveInterval != effectiveInterval)
     return "Time info not collected";
 
-  return (effectiveInterval).toFixed(2) + " ms +-" + biggestDiff.toFixed(2);
+  return (effectiveInterval).toFixed(2) + " ms ±" + biggestDiff.toFixed(2);
 }
 
 function numberOfCurrentlyShownSamples() {
@@ -1048,8 +1048,8 @@ InfoBar.prototype = {
   },
   display: function InfoBar_display() {
     function getMetaFeatureString() {
-      features = "Stackwalk: " + (gMeta.stackwalk ? "True" : "False");
-      features += ", Jank: " + (gMeta.stackwalk ? "True" : "False");
+      features = "<dt>Stackwalk:</dt><dd>" + (gMeta.stackwalk ? "True" : "False") + "</dd>";
+      features += "<dt>Jank:</dt><dd>" + (gMeta.stackwalk ? "True" : "False") + "</dd>";
       return features;
     }
     function getPlatformInfo() {
@@ -1057,30 +1057,30 @@ InfoBar.prototype = {
     }
     var infobar = this._container;
     var infoText = "";
-    
+
     if (gMeta) {
-      infoText += "<h2>Profile Info</h2>\n";
-      infoText += "Product: " + gMeta.product + "<br>";
-      infoText += "Platform: " + getPlatformInfo() + "<br>";
-      infoText += getMetaFeatureString() + "<br>";
-      infoText += "Interval: " + gMeta.interval + " ms<br>";
+      infoText += "<h2>Profile Info</h2>\n<dl>\n";
+      infoText += "<dt>Product:</dt><dd>" + gMeta.product + "</dd>";
+      infoText += "<dt>Platform:</dt><dd>" + getPlatformInfo() + "</dd>";
+      infoText += getMetaFeatureString();
+      infoText += "<dt>Interval:</dt><dd>" + gMeta.interval + " ms</dd></dl>";
     }
-    infoText += "<h2>Selection Info</h2>\n<ul>\n";
-    infoText += "  <li>Avg. Responsiveness:<br>" + avgResponsiveness().toFixed(2) + "ms</li>\n";
-    infoText += "  <li>Max Responsiveness:<br>" + maxResponsiveness().toFixed(2) + "ms</li>\n";
-    infoText += "  <li>Real Interval: " + effectiveInterval() + "</li>";
-    infoText += "</ul>\n";
+    infoText += "<h2>Selection Info</h2>\n<dl>\n";
+    infoText += "  <dt>Avg. Responsiveness:</dt><dd>" + avgResponsiveness().toFixed(2) + " ms</dd>\n";
+    infoText += "  <dt>Max Responsiveness:</dt><dd>" + maxResponsiveness().toFixed(2) + " ms</dd>\n";
+    infoText += "  <dt>Real Interval:</dt><dd>" + effectiveInterval() + "</dd>";
+    infoText += "</dl>\n";
     infoText += "<h2>Pre Filtering</h2>\n";
     // Disable for now since it's buggy and not useful
     //infoText += "<label><input type='checkbox' id='mergeFunctions' " + (gMergeFunctions ?" checked='true' ":" ") + " onchange='toggleMergeFunctions()'/>Functions, not lines</label><br>\n";
     infoText += "<label><input type='checkbox' id='showJS' " + (gJavascriptOnly ?" checked='true' ":" ") + " onchange='toggleJavascriptOnly()'/>Javascript only</label><br>\n";
 
     var filterNameInputOld = document.getElementById("filterName");
-    infoText += "Filter:\n";
-    infoText += "<input type='text' id='filterName' oninput='filterOnChange()'/><br>\n";
+    infoText += "<label>Filter:\n";
+    infoText += "<input type='search' id='filterName' oninput='filterOnChange()'/></label>\n";
 
     infoText += "<h2>Post Filtering</h2>\n";
-    infoText += "<label><input type='checkbox' id='showJank' " + (gJankOnly ?" checked='true' ":" ") + " onchange='toggleJank()'/>Show Jank only</label><br>\n";
+    infoText += "<label><input type='checkbox' id='showJank' " + (gJankOnly ?" checked='true' ":" ") + " onchange='toggleJank()'/>Show Jank only</label>\n";
     infoText += "<h2>View Options</h2>\n";
     infoText += "<label><input type='checkbox' id='mergeUnbranched' " + (gMergeUnbranched ?" checked='true' ":" ") + " onchange='toggleMergeUnbranched()'/>Merge unbranched call paths</label><br>\n";
     infoText += "<label><input type='checkbox' id='invertCallstack' " + (gInvertCallstack ?" checked='true' ":" ") + " onchange='toggleInvertCallStack()'/>Invert callstack</label><br>\n";
@@ -1089,7 +1089,7 @@ InfoBar.prototype = {
     infoText += "<div id='upload_status' aria-live='polite'>No upload in progress</div><br>\n";
     infoText += "<input type='button' id='upload' value='Upload full profile'>\n";
     infoText += "<input type='button' id='upload_select' value='Upload view'><br>\n";
-    infoText += "<input type='button' id='download' value='Download full profile'><br>\n";
+    infoText += "<input type='button' id='download' value='Download full profile'>\n";
 
     //infoText += "<br>\n";
     //infoText += "Skip functions:<br>\n";
