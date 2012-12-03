614602213c27719b0a28cd1fa8c6632b1fc7214b
Benoit Girard <bgirard@mozilla.com>
2012-12-03 13:37:45 -0500

    Disable the compare feature to deploy the current updates

--- js/ui.js
+++ js/ui.js
@@ -1231,8 +1231,10 @@ InfoBar.prototype = {
     infoText += "<label><input type='checkbox' id='mergeUnbranched' " + (gMergeUnbranched ?" checked='true' ":" ") + " onchange='toggleMergeUnbranched()'/>Merge unbranched call paths</label><br>\n";
     infoText += "<label><input type='checkbox' id='invertCallstack' " + (gInvertCallstack ?" checked='true' ":" ") + " onchange='toggleInvertCallStack()'/>Invert callstack</label><br>\n";
 
-    infoText += "<h2>Compare</h2>\n";
-    infoText += "<input type='button' id='compare' value='Compare'>\n";
+    // Not yet ready
+    //infoText += "<h2>Compare</h2>\n";
+    //infoText += "<input type='button' id='compare' value='Compare'>\n";
+
     infoText += "<h2>Share</h2>\n";
     infoText += "<div id='upload_status' aria-live='polite'>No upload in progress</div><br>\n";
     infoText += "<input type='button' id='upload' value='Upload full profile'>\n";
