2cf3718eed1ff43fab0e7960e27ae1d4ed2ce90e
Benoit Girard <bgirard@mozilla.com>
2012-12-07 15:06:25 -0500

    Stop updating the URL bar because url are too long

--- js/ui.js
+++ js/ui.js
@@ -1923,7 +1923,7 @@ function queryEscape(str) {
 }
 
 function updateDocumentURL() {
-  location.hash = getDocumentHashString();
+  //location.hash = getDocumentHashString();
   return document.location;
 }
 
