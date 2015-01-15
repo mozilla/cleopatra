5afb8dd3e799615e32ff4a0c2cf22624bfde0472
Benoit Girard <b56girard@gmail.com>
2015-01-14 20:09:12 -0500

    Polyfill CustomEvent for PhantomJS

--- js/ui.js
+++ js/ui.js
@@ -154,3 +154,18 @@ if (!Function.prototype.bind) {
   };
 }
 
+if (!window.CustomEvent || typeof window.CustomEvent !== 'function') {
+  var CustomEvent = function(event, params) {
+    var evt;
+    params = params || {
+      bubbles: false,
+      cancelable: false,
+      detail: undefined
+    };
+    evt = document.createEvent("CustomEvent");
+    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+    return evt;
+  };
+  CustomEvent.prototype = window.Event.prototype;
+  window.CustomEvent = CustomEvent; // expose definition to window
+}
