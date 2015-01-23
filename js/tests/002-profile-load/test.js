a89e5394c09f470076d0e17041622332600d44a0
Benoit Girard <b56girard@gmail.com>
2015-01-23 11:45:37 -0500

    Add a test for loading talos zips

--- js/tests/002-profile-load/test.js
+++ js/tests/002-profile-load/test.js
@@ -5,7 +5,7 @@ QUnit.test("Cleopatra Simple Profile", function(assert) {
     testFunc: function(cleopatraDocument) {
     },
     profileLoadFunc: function(cleopatraObj) {
-      assert.ok(cleopatraObj.window.gNumSamples === 174, "Loaded profile");
+      assert.equal(cleopatraObj.window.gNumSamples, 174, "Loaded profile");
     },
   });
 });
@@ -17,7 +17,19 @@ QUnit.test("Cleopatra Complex Profile", function(assert) {
     testFunc: function(cleopatraObj) {
     },
     profileLoadFunc: function(cleopatraObj) {
-      assert.ok(cleopatraObj.window.gNumSamples === 558, "Loaded profile");
+      assert.equal(cleopatraObj.window.gNumSamples, 558, "Loaded profile");
+    },
+  });
+});
+
+QUnit.test("Cleopatra Zip (Talos) Profile", function(assert) {
+  loadCleopatra({
+    query: "?zippedProfile=http://mozilla-releng-blobs.s3.amazonaws.com/blobs/Try-Non-PGO/sha512/02ce11479d9a0c03eee146c9ff18010e9eca892dd9b3ab92eed40f07fcb295a1c4a2e5ed317bbf784e0363f4f3630c4cb7117e7522e21c6d2b48fb469ed68cd5&pathInZip=profile_tresize/tresize/cycle_7.sps",
+    assert: assert,
+    testFunc: function(cleopatraObj) {
+    },
+    profileLoadFunc: function(cleopatraObj) {
+      assert.equal(cleopatraObj.window.gNumSamples, 2646, "Loaded profile");
     },
   });
 });
