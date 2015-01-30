247e01afc94a52503ddb1aeff842f445e296faa7
Benoit Girard <b56girard@gmail.com>
2015-01-30 16:15:09 -0500

    fix portability issue with find not accepting file

--- appcache_generator.sh
+++ appcache_generator.sh
@@ -3,7 +3,7 @@ echo "CACHE MANIFEST" > cleopatra.appcache
 echo "# $(date)" >> cleopatra.appcache
 echo "" >> cleopatra.appcache
 echo "CACHE:" >> cleopatra.appcache
-find *.html js css images -not -path '*/\.*' -type file >> cleopatra.appcache
+find *.html js css images -not -path '*/\.*' -type f >> cleopatra.appcache
 echo "" >> cleopatra.appcache
 echo "NETWORK:" >> cleopatra.appcache
 echo "*" >> cleopatra.appcache
